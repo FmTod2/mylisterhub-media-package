9619f41f5ee1c10e98e9b23cc30b8d3dabee5fca
Julio Duque <juliocd94@gmail.com>
2025-01-28 14:56:04 +0000

    feat: add properties to video factory

--- database/factories/VideoFactory.php
+++ database/factories/VideoFactory.php
@@ -15,7 +15,10 @@ class VideoFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'name' => $this->faker->sentence(3),
+            'path' => $this->faker->filePath(),
+            'disk' => $this->faker->randomElement(['public', 's3']),
+            'url' => $this->faker->url()
         ];
     }
 }
